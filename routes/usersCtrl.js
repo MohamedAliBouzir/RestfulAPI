11e4af83758a1bf7748570c612c89465ec38d4f2
MohamedAliBouzir <bzr.medali@gmail.com>
2022-05-09 09:54:24 +0100

    fixed login bug

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -3,6 +3,10 @@ var bcrypt =  require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
 
+//consts
+const EMAIL_REGEX     = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX  = /^(?=.*\d).{4,8}$/;
+
 //Routes
 module.exports = {
     register: function(req,res) {
@@ -14,6 +18,15 @@ module.exports = {
 
         if (email == null || username == null || password == null){
             return res.status(400).json({ 'error': 'missing parameters'});
+        }
+        if(username.length >= 13 || username.length <= 4){
+            return res.status(400).json({'error':'wrong username ( username length must be between 5 and 12 )'});
+        }
+        if(!EMAIL_REGEX.test(email)){
+            return res.status(400).json({'error': 'email is not valid'});
+        }
+        if(!PASSWORD_REGEX.test(password)){
+            return res.status(400).json({'error': 'password is not valid'});
         }
         //verify pseudo length, mail regex, password
         models.User.findOne({
@@ -53,10 +66,11 @@ module.exports = {
         //to implement
         var email    = req.body.email;
         var password = req.body.password;
-
+        //conditions
         if (email == null ||  password == null) {
             return res.status(400).json({ 'error': 'missing parameters' });
         }
+        
         //verify email regex password length
         models.User.findOne({
             where: {email: email}
