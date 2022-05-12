d656a0362bd6986e5e568b87b1681152249e90a6
MohamedAliBouzir <bzr.medali@gmail.com>
2022-05-12 09:06:29 +0100

    fixed header and authorazation problem

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ var apiRouter = require('./apiRouter').router;
 // instantiate server
 var server = express();
 
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Access-Control-Allow-Headers, https://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, ');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+      res.header('Access-Control-Allow-Credentials', true);
+      return res.status(200).json({});
+
+  };
+    next();
+  });
+
 //body parser configuration
 server.use(bodyParser.urlencoded({ extended:true}));
 server.use(bodyParser.json());
